79c6b484d6897fafec1df29c14ffea2f30220581
Hans Micaya <hcmicaya@gmail.com>
2022-04-06 22:22:41 -0400

    setup api redux, form, and createPost

--- server/index.js
+++ server/index.js
@@ -7,16 +7,19 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
+const CONNECTION_URL =
+    "mongodb+srv://hansmicaya:hansmicaya123@cluster0.eqnts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 mongoose
-    .connect(process.env.CONNECTION_URL, {})
+    .connect(CONNECTION_URL, {})
     .then(() =>
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
     )
