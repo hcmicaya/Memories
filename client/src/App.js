0cc8920c6a6f41bc8977cbb2a3cec0f3929660af
Hans Micaya <hcmicaya@gmail.com>
2022-04-09 17:07:02 -0400

    Add posts card, update, delete and like. Fix mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
@@ -9,11 +9,12 @@ import memories from "./images/memories.png";
 import useStyles from "./styles";
 
 const App = () => {
-    const classes = useStyles();
+    const [currentId, setCurrentId] = useState(null);
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     useEffect(() => {
-        dispatch(getPosts);
+        dispatch(getPosts());
     }, [dispatch]);
 
     return (
@@ -40,16 +41,20 @@ const App = () => {
             <Grow in>
                 <Container>
                     <Grid
+                        className={classes.mainContainer}
                         container
                         justify="space-between"
                         alignItems="stretch"
                         spacing={3}
                     >
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form
+                                currentId={currentId}
+                                setCurrentId={setCurrentId}
+                            />
                         </Grid>
                     </Grid>
                 </Container>
--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import * as api from "../api";
+import * as api from "../api/index.js";
 
 // Action Creators
 export const getPosts = () => async (dispatch) => {
@@ -16,6 +16,36 @@ export const createPost = (post) => async (dispatch) => {
         const { data } = await api.createPost(post);
 
         dispatch({ type: "CREATE", payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: "UPDATE", payload: data });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: "DELETE", payload: id });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: "LIKE", payload: data });
     } catch (error) {
         console.log(error);
     }
