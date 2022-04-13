14cdb8c136a74b26367fbd4412bcf18bbc5d6b13
Hans Micaya <hcmicaya@gmail.com>
2022-04-12 20:29:13 -0400

    Add pagination component, fix App.js routes, and search bar in home

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import PostDetails from "./components/PostDetails/PostDetails";
 import Auth from "./components/Auth/Auth";
 
-const App = () => (
-    <BrowserRouter>
-        <Container maxidth="lg">
-            <Navbar />
-            <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/auth" exact element={<Auth />} />
-            </Routes>
-        </Container>
-    </BrowserRouter>
-);
+const App = () => {
+    const user = JSON.parse(localStorage.getItem("profile"));
+
+    return (
+        <BrowserRouter>
+            <Container maxidth="xl">
+                <Navbar />
+                <Routes>
+                    <Route
+                        path="/"
+                        exact
+                        element={<Navigate replace to="/posts" />}
+                    />
+                    <Route path="/posts" exact element={<Home />} />
+                    <Route path="/posts/search" exact element={<Home />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route
+                        path="/auth"
+                        exact
+                        element={
+                            !user ? <Auth /> : <Navigate replace to="/posts" />
+                        }
+                    />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    );
+};
 
 export default App;
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PostDetails = () => {
+    console.log("POST DETAILS");
+
+    return <div>PostDetails</div>;
+};
+
+export default PostDetails;
